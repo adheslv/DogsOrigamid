5f31c0abda207c6ebce38d45e238424500e2c0e4
adheslv <adhe.asf@outlook.com>
2025-08-01 20:04:27 -0300

    Ajuste no Login form e estilização do login background

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -5,8 +5,13 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ,
     message: 'Preencha um email válido',
-  }
-}
+  },
+  password: {
+    regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres.',
+  },
+};
+
 const useForm = (type) => {
   const [value,setValue] = React.useState('');
   const [error,setError] = React.useState(null);
