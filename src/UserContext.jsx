50ac1331f0042b2207066fa69c27b50c90606181
adheslv <adhe.asf@outlook.com>
2025-08-05 15:03:19 -0300

    Criação e customização do menu do usuário no cabeçalho.

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -18,10 +18,7 @@ export const UserStorage = ({children}) => {
       setLoading(false);
       setLogin(false);
       window.localStorage.removeItem('token');
-      navigate('/login');
-    },
-    [navigate],
-  );
+    },[],);
 
   async function getUser(token) {
     const {url, options} = USER_GET(token);
@@ -66,6 +63,8 @@ export const UserStorage = ({children}) => {
         } finally {
           setLoading(false);
         }
+      } else {
+        setLogin(false);
       }
     }
     autoLogin()
