a160866c4c15851f8e9cf121d4850c9e93ab4736
adheslv <adhe.asf@outlook.com>
2025-08-06 22:41:35 -0300

    Criação do FeedPhoto e estilização

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
   return <div>
-    Feed
+    <FeedModal />
+    <FeedPhotos />
   </div>;
 };
 
